7ff8b0f1fd9806a6fae007c400b04f61db4abea5
Chris <Christopher.a.mancuso@gmail.com>
2017-02-13 00:48:52 -0500

    Last commit for the night

--- app/data_pull.js
+++ app/data_pull.js
@@ -4,32 +4,50 @@ var quandl = new Quandl({
     api_version: 3,
 });
 
+	var f_shares = 19455.2529  		//$200k/10.28
+	var ba_shares = 3559.9858  		//$200k/56.18
+	var vz_shares = 6009.6154  		//$200k/33.28
+	var ge_shares = 12944.9838  	//$200k/15.45
+	var goog_shares = 757.6332  	//$200k/263.98
 
-
-var pullData = function(){
-	quandl.dataset({
-	source: "WIKI",
-	table: "FB"
-	}, {
-	order: "asc",
-	exclude_column_names: true,
-	// Notice the YYYY-MM-DD format
-	  start_date: "2010-01-01",
-	  end_date: "2017-02-12",
-	  column_index: 4
-	}, function(err, response){
-	    if(err)
-	        throw err;
-
-	    console.log(response);
-	});
+var pullData = function(stocks,shares){
+	finalarray = {id:[{}]};
+	for(var j= 0; j<(stocks).length; j++){
+		var quantity = shares[j]
+		console.log(quantity)
+		console.log(stocks[j])
+		quandl.dataset({
+		source: "WIKI",
+		table: stocks[j],
+		}, {
+		order: "asc",
+		exclude_column_names: false,
+		// Notice the YYYY-MM-DD format
+		  start_date: "2010-01-01",
+		  end_date: "2017-02-12",
+		  column_index: 4
+		}, function(err, response){
+		    if(err)
+		        throw err;
+		    output = JSON.parse(response);
+		    adjustedarray = {id:[{}]};
+		    for(var i=0; i<(output.dataset.data).length; i++){
+		    	adjustedarray.id = i;
+		    	adjustedarray[i] = {date: output.dataset.data[i][0], value: output.dataset.data[i][1]*quantity}
+		    	if(j==0){
+		    		finalarray.id = i;
+		    		finalarray[i] = {date: output.dataset.data[i][0], value: output.dataset.data[i][1]*quantity}
+		    	}
+		    	else{
+		    		// finalarray[i]['value']+=adjustedarray.value;
+		    	}
+		    }
+		    // console.log(adjustedarray)
+		});
+	}
+	// console.log(finalarray);
 }
 
-quandl.metadata("ZILLOW", "ZIP_ALLHOMES_15235", function(err, response){
-    if(err)
-        throw err;
 
-    console.log(response);
-});
 
 module.exports = pullData;
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,15 @@
 
-
 var pullData = require('../app/data_pull.js')
 var createChart = require('../app/chart.js')
+var test = require('../app/hc-test.js')
 
 
 exports.index = function(req, res){
-	$ = require('cheerio');
-	var Highcharts = require('highcharts/highstock');
-	var data = [1,2,3,4];
-	Highcharts.chart('stockTable', {
-
-
-        rangeSelector: {
-            selected: 1
-        },
-
-        title: {
-            text: 'AAPL Stock Price'
-        },
-
-        series: [{
-            name: 'AAPL',
-            data: data,
-            tooltip: {
-                valueDecimals: 2
-            }
-        }]
-    });
-	// pullData();
-	// createChart();
 	res.render('index');
+
+	var shares= [19455.2529,3559.9858,6009.6154,12944.9838,757.6332];
+	var stocks = ['F','BA','VZ','GE','GOOG']; //very easy to make this a dynamic array
+	pullData(stocks, shares);
+	// createChart();
+	
 };
